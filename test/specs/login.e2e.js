0b471ce63b0e249c569c161d95ad6c04b356ea28
tatiana.utrera <tatiana.utrera@5by5.com.br>
2025-07-16 21:23:35 -0300

    ajustes delay

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -10,7 +10,7 @@ describe('Login test', () => {
     })
     it.only('When fill valid login, Should open the app', async () => {
         await LoginScreen.login(login.ValidLogin[0])
-        if(await AppointmentsScreen.btnPermission.isEnabled()){
+        if(await AppointmentsScreen.btnPermission.isDisplayed()){
              await LoginScreen.acceptAlert()
         } 
         await LoginScreen.assertEnabled(AppointmentsScreen.txtAgenda)
@@ -42,7 +42,7 @@ describe('Login test', () => {
     })
     it('When fill valid login and check de option save login, Should login and save the credencials', async () => {
         await LoginScreen.login(login.ValidLoginWithSaveLogin[0])
-        if(await AppointmentsScreen.btnPermission.isEnabled()) 
+        if(await AppointmentsScreen.btnPermission.isDisplayed()) 
             await LoginScreen.acceptAlert()
         await LoginScreen.assertEnabled(AppointmentsScreen.txtAgenda)
         await AppointmentsScreen.finishSession()
